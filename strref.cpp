b39b452890cab2fb6eea8bdf2ce6bfb9689d523e
yang acer <yangacer@gmail.com>
2011-05-14 03:09:41 +0800

    Add strref class

---
+++ strref.cpp
@@ -0,0 +1,187 @@
+#include "strref.h"
+#include <cstring>
+#include <algorithm>
+#include <functional>
+#include <ostream>
+
+str_ref::str_ref()
+: data_(0), size_(0)
+{}
+
+str_ref::str_ref(char const *data, unsigned int size)
+: data_(data), size_(size)
+{}
+
+str_ref::str_ref(char const* data_beg, char const* data_end)
+: data_(data_beg), size_(data_end - data_beg)
+{}
+
+str_ref&
+str_ref::assign(char const* data, unsigned int size)
+{	data_ = data; size_ = size;	}
+
+str_ref&
+str_ref::assign(char const* data_beg, char const* data_end)
+{	data_ = data_beg; size_ = data_end - data_beg;	}
+
+char const*
+str_ref::data() const
+{ return data_; }
+
+unsigned int
+str_ref::size() const
+{ return size_; }
+
+void
+str_ref::clear()
+{ data_ = 0; size_ = 0; }
+
+bool 
+str_ref::operator>(str_ref const& rhs) const
+{
+	return std::lexicographical_compare(rhs.data_, rhs.data_ + rhs.size_, data_, data_ + size_);
+}
+
+bool 
+str_ref::operator<(str_ref const& rhs) const
+{
+
+	return std::lexicographical_compare(data_, data_ + size_, rhs.data_, rhs.data_ + rhs.size_);
+}
+
+bool 
+str_ref::operator>=(str_ref const& rhs) const
+{	return !(*this<rhs);	}
+
+bool 
+str_ref::operator<=(str_ref const& rhs) const
+{	return !(*this>rhs);	}
+
+bool 
+str_ref::operator==(str_ref const& rhs) const
+{
+	return rhs.size_ == size_ && strncmp(data_, rhs.data_, size_);	
+}
+
+bool 
+str_ref::operator!=(str_ref const& rhs) const
+{
+	return !(*this == rhs);	
+}
+
+// -------------- comparison between std::string
+bool 
+str_ref::operator>(std::string const& rhs) const
+{
+	return std::lexicographical_compare(rhs.begin(), rhs.end(), data_, data_ + size_);
+}
+
+bool 
+str_ref::operator<(std::string const& rhs) const
+{
+
+	return std::lexicographical_compare(data_, data_ + size_, rhs.begin(), rhs.end());
+}
+
+bool 
+str_ref::operator>=(std::string const& rhs) const
+{	return !(*this<rhs);	}
+
+bool 
+str_ref::operator<=(std::string const& rhs) const
+{	return !(*this>rhs);	}
+
+bool 
+str_ref::operator==(std::string const& rhs) const
+{
+	return rhs.size() == size_ && strncmp(data_, rhs.c_str(), size_);	
+}
+
+bool 
+str_ref::operator!=(std::string const& rhs) const
+{
+	return !(*this == rhs);	
+}
+
+//std::istream &operator>>(std::istream& is, str_ref const& sref)
+//{ return is;}
+
+std::ostream &operator<<(std::ostream& os, str_ref const& sref)
+{
+	os.write(sref.data_, sref.size_);	
+	return os;
+}
+
+/*
+unsigned int
+referenced_count(record const& r)
+{
+	record::const_iterator iter = r.const_begin();
+	unsigned int size(0);
+	field<str_ref> *p(0);
+	while(iter != r.const_end()){
+		p = dynamic_cast<field<str_ref>*>(*iter);
+		if(p)
+			size += p->val_.size_;
+		++iter;
+	}
+	return size;
+}
+
+
+void
+cp_strref(std::string &buffer, record &r)
+{
+	record::iterator iter = r.begin();
+	field<str_ref> *p(0);
+	while(iter != r.end()){
+		p = dynamic_cast<field<str_ref>*>(*iter);
+		if(p && p->val_.size_)
+			buffer.append(p->val_.data_, p->val_.size_);
+		++iter;
+	}
+	
+}
+
+void
+rebuild_ref(std::string &buffer, record *beg, record *end)
+{
+	record *iter = beg;
+	record::iterator fIter;
+	field<str_ref> *p(0);
+	char const *data = buffer.data();
+	unsigned int curPos(0);
+
+	while(iter != end){
+		fIter = iter->begin();
+		while(fIter != iter->end()){
+			p = dynamic_cast<field<str_ref>*>(*fIter);
+			if(p && p->val_.size_){
+				p->val_.data_ = data + curPos;
+				curPos += p->val_.size_;
+			}
+			++fIter;	
+		}
+		++iter;	
+	}
+}
+
+unsigned int
+cp_chg_referenced(char *buf, record &r)
+{
+	record::iterator iter = r.begin();
+	unsigned int size(0);
+	field<str_ref> *p(0);
+	while(iter != r.end()){
+		p = dynamic_cast<field<str_ref>*>(*iter);
+		if(p){
+			memcpy(buf + size, p->val_.data_, p->val_.size_);
+			//(*iter)->fromString(buf+size, p->val_.size_);
+			p->val_.data_ = buf + size;
+			size += p->val_.size_;
+		}
+		++iter;
+	}
+	return size;
+}
+*/
---
+++ strref.h
@@ -0,0 +1,168 @@
+#ifndef _STRREF_H
+#define _STRREF_H
+
+#include <iosfwd>
+#include <cstring>
+#include <string>
+
+#define SIX_BOOL_OP_DECL(C,X) bool operator X (C const& rhs)const
+#define GLOBAL_SBOP_FUNC(T,U,X,Y) bool operator X \
+	(T const &lhs, U const &rhs){ return rhs Y lhs; }
+
+namespace Yaks
+{
+
+	struct str_ref
+	{
+		str_ref();
+		str_ref(char const* data, unsigned int size);
+		str_ref(char const* data_beg, char const* data_end);
+		
+		SIX_BOOL_OP_DECL(str_ref,>);
+		SIX_BOOL_OP_DECL(str_ref,<);
+		SIX_BOOL_OP_DECL(str_ref,>=);
+		SIX_BOOL_OP_DECL(str_ref,<=);
+		SIX_BOOL_OP_DECL(str_ref,==);
+		SIX_BOOL_OP_DECL(str_ref,!=);
+		
+		SIX_BOOL_OP_DECL(std::string,>);
+		SIX_BOOL_OP_DECL(std::string,<);
+		SIX_BOOL_OP_DECL(std::string,>=);
+		SIX_BOOL_OP_DECL(std::string,<=);
+		SIX_BOOL_OP_DECL(std::string,==);
+		SIX_BOOL_OP_DECL(std::string,!=);
+
+		str_ref&
+		assign(char const* data, unsigned int size);
+
+		str_ref&
+		assign(char const* data_beg, char const* data_end);
+
+		void
+		clear();
+
+		char const* data() const;
+
+		unsigned int size() const;
+
+		char const* data_;
+		unsigned int size_;
+	};
+
+	inline GLOBAL_SBOP_FUNC(std::string,str_ref,>,<)
+	inline GLOBAL_SBOP_FUNC(std::string,str_ref,<,>)
+	inline GLOBAL_SBOP_FUNC(std::string,str_ref,>=,<=)
+	inline GLOBAL_SBOP_FUNC(std::string,str_ref,<=,>=)
+	inline GLOBAL_SBOP_FUNC(std::string,str_ref,==,==)
+	inline GLOBAL_SBOP_FUNC(std::string,str_ref,!=,!=)
+
+
+	std::ostream &operator<<(std::ostream& os, str_ref const& sref);
+
+} // end of Yaks namespace
+
+/*
+unsigned int
+referenced_count(record const& r);
+
+unsigned int
+cp_chg_referenced(char *buf, record &r);
+
+void
+cp_strref(std::string &buffer, record &r);
+
+void
+rebuild_ref(std::string &buffer, record *beg, record *end);
+
+template<>
+class field<str_ref> : public absField, public Loki::SmallObject<>
+{
+	friend unsigned int referenced_count(record const& r);
+	friend unsigned int cp_chg_referenced(char *buf, record &r);
+	friend void cp_strref(std::string &buffer, record &r);
+	friend void rebuild_ref(std::string &buffer, record *beg, record *end);
+
+private: // Client never creates field object directly
+	friend class record;
+	template<class T1> friend struct create_field;
+
+	typedef str_ref value_type;
+
+	field()
+	: val_()
+	{}
+
+	field(field const &cp)
+	: val_(cp.val_)
+	{}
+
+	~field()
+	{}
+
+	field* 
+	Clone() const
+	{ return new field(*this); }
+
+	
+	void
+	Clone(absField* f) const
+	{ new (f) field<str_ref>(*this); }
+	
+
+	int 
+	compare(absField const* rhs, bool sameType = false) const throw(char const*)
+	{
+		field* p = (sameType) ? 
+			static_cast<field*>(const_cast<absField*>(rhs))
+			: dynamic_cast<field*>(	const_cast<absField*>(rhs));
+
+		if(0 == p)
+			return -2;
+		if(val_ < p->val_)
+			return -1;
+		if(val_ > p->val_)
+			return 1;
+		return 0;
+	}
+	
+
+	int 
+	compare(absField const* rhs, void* compare, bool sameType = false) const throw(char const*)
+	{
+		return this->compare(rhs, sameType);	
+	}
+
+	bool 
+	fromString(char const *cstr, size_t size)
+	{
+		val_.data_ = cstr;
+		val_.size_ = size;
+		return true;
+	}
+
+	bool 
+	fromString(char const *str)
+	{
+		val_.data_ = str;
+		val_.size_ = std::strlen(str);
+		return true;
+	}
+
+
+	std::string 
+	toString() const 
+	{
+		return std::string(val_.data_, val_.size_);
+	}
+
+	std::ostream&
+	writeTo(std::ostream& os) const
+	{	
+		os.write(val_.data_, val_.size_);
+		return os;
+	}
+
+	str_ref val_;
+};
+*/
+#endif // end of header guard
